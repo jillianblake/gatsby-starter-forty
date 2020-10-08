680e2af7b82370779cf4a0acaafcbad618c1e031
jillianblake <71796537+jillianblake@users.noreply.github.com>
2020-10-08 17:46:09 -0400

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,12 +4,12 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, welcome to Blake Immigration Law.</h1>
+                <h1>Hello, welcome to Blake Immigration Law.</h1>
             </header>
             <div className="content">
                 <p>WE SPECIALIZE IN REMOVAL DEFENSE, ASYLUM, AND FAMILY-BASED IMMIGRATION.</p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                    <li><a href="#one" className="button next scrolly">Contact Us</a></li>
                 </ul>
             </div>
         </div>
