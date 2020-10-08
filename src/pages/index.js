556b6cae4fd604faec93e4809b4fbdce294cbadb
jillianblake <71796537+jillianblake@users.noreply.github.com>
2020-10-08 19:56:45 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ class HomeIndex extends React.Component {
                     <section id="one" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
-                                <h3>About Us</h3>
+                                <h3>About Us Testing</h3>
                                 <p>Meet Our Team</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
