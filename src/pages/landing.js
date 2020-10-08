4587290e5c937b8639ba36767b23c965e7d1f022
jillianblake <71796537+jillianblake@users.noreply.github.com>
2020-10-08 18:46:35 -0400

    Update landing.js

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -12,7 +12,7 @@ const Landing = (props) => (
     <Layout>
         <Helmet>
             <title>About Us</title>
-            <meta name="description" content="Landing Page" />
+            <meta name="description" content="About Us" />
         </Helmet>
 
         <BannerLanding />
@@ -22,23 +22,21 @@ const Landing = (props) => (
                 <div className="inner">
                     <header className="major">
                         <h2>Sed amet aliquam</h2>
-                    </header>
-                    <p>WE ARE A GROUP OF HARD-WORKING LEGAL PROFESSIONALS WHO HANDLE EACH CASE WITH COMPASSION, DILIGENCE, AND INTEGRITY.</p>
-                </div>
-            </section>
-            <section id="two" className="spotlights">
-                <section>
+                   
                     <Link to="/generic" className="image">
                         <img src={pic08} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Orci maecenas</h3>
+                                <h3>Jillian Blake, Esq.</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+     <header className="major">
+                                <h3>Attorney/Owner</h3>
+                            </header>
+                            <p>Jillian Blake is an attorney and owner of Blake Immigration Law since 2015. She grew up in San Francisco, California and earned her bachelor’s degree in international studies from Johns Hopkins University and master’s degree in international relations from the Johns Hopkins School of Advanced International Studies (SAIS). She then obtained her law degree from the University of Michigan Law School. During law school she discovered her passion for immigration and asylum law while interning at Asylum Access—Ecuador and Capital Area Immigrants’ Rights (CAIR) Coalition. Jillian is a member of the Maryland State Bar and the American Immigration Lawyers Association (AILA). She is also currently an adjunct professor of immigration law at George Mason University Law School in Arlington, Virginia. In her free time Jillian enjoys watching movies, writing, watching Cleveland Browns football and Nationals baseball, and spending time with her dog, Cha Cha.. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
                             <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
+                                
                             </ul>
                         </div>
                     </div>
